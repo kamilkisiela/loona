b1b49b1ea65bda493623e8dd48b986c3665b4484
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-09-03 19:49:37 +0200

    Remove some code

--- packages/core/src/internal/store.ts
+++ packages/core/src/internal/store.ts
@@ -13,22 +13,6 @@ export class Store<T> {
     this.store.set(key, value);
   }
 
-  delete(key: string): void {
-    this.store.delete(key);
-  }
-
-  clear(): void {
-    return this.store.clear();
-  }
-
-  replace(newData: Record<string, T>): void {
-    this.store.clear();
-
-    Object.entries(newData).forEach(([key, value]) =>
-      this.store.set(key, value),
-    );
-  }
-
   forEach(fn: (value: T, key: string) => void) {
     this.store.forEach(fn);
   }
