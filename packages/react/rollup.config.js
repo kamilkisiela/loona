e4efa9582b2d2b3600c44f5ddf649a55b455f9d6
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-09-07 22:33:27 +0200

    Add bundle size checking in react

--- packages/react/rollup.config.js
+++ packages/react/rollup.config.js
@@ -1,16 +1,20 @@
+import {uglify} from 'rollup-plugin-uglify';
+
 const globals = {
   react: 'React',
   'react-apollo': 'react-apollo',
   'apollo-client': 'apollo',
   '@loona/core': 'loona.core',
-  'prop-types': 'PropTypes'
+  'prop-types': 'PropTypes',
 };
 
-export default {
+const file = 'build/loona.react.umd.js';
+
+const config = {
   input: 'build/index.js',
   external: Object.keys(globals),
   output: {
-    file: 'build/loona.react.umd.js',
+    file,
     format: 'umd',
     name: 'loona.react',
     exports: 'named',
@@ -19,6 +23,18 @@ export default {
   },
   onwarn,
 };
+
+export default [
+  config,
+  {
+    ...config,
+    output: {
+      ...config.output,
+      file: file.replace('.js', '.min.js'),
+    },
+    plugins: [uglify()],
+  },
+];
 
 function onwarn(message) {
   const suppressed = ['THIS_IS_UNDEFINED'];
