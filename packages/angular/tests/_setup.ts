e550e67e7605ecf93c9620081e1757fce2b27d4a
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-09-08 21:00:27 +0200

    test(angular): integration of module

---
+++ packages/angular/tests/_setup.ts
@@ -0,0 +1,16 @@
+import 'reflect-metadata';
+import 'zone.js/dist/zone-node';
+import 'zone.js/dist/proxy.js';
+import 'zone.js/dist/sync-test';
+import 'zone.js/dist/async-test';
+import 'zone.js/dist/fake-async-test';
+import {getTestBed} from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting,
+} from '@angular/platform-browser-dynamic/testing';
+
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting(),
+);
