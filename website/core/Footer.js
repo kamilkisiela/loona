0e2894eb9143351ead51f49a81109311743ebb76
Kamil Kisiela <kamil.kisiela@gmail.com>
2018-09-28 19:59:21 +0200

    Favicon, footer

--- website/core/Footer.js
+++ website/core/Footer.js
@@ -21,47 +21,37 @@ class Footer extends React.Component {
   render() {
     return (
       <footer className="nav-footer" id="footer">
-        <section className="sitemap">
-          <a href={this.props.config.baseUrl} className="nav-home">
-            {this.props.config.footerIcon && (
-              <img
-                src={this.props.config.baseUrl + this.props.config.footerIcon}
-                alt={this.props.config.title}
-                width="66"
-                height="58"
-              />
-            )}
-          </a>
+        <section className="footer-brand">
           <div>
-            <h5>Docs</h5>
-            <a href={this.docUrl('angular', this.props.language)}>
-              Angular
+            <a href={this.props.config.baseUrl} className="nav-home">
+              {this.props.config.footerIcon && (
+                <img
+                  src={this.props.config.baseUrl + this.props.config.footerIcon}
+                  alt={this.props.config.title}
+                />
+              )}
             </a>
-            <a href={this.docUrl('react', this.props.language)}>
-              React
-            </a>
+            <span className="copyrights">{this.props.config.copyright}</span>
+          </div>
+        </section>
+        <section className="sitemap">
+          <div>
+            <h5>Documentation</h5>
+            <a href={this.docUrl('angular', this.props.language)}>Angular</a>
+            <a href={this.docUrl('react', this.props.language)}>React</a>
           </div>
           <div>
             <h5>Community</h5>
             <a
               href="http://stackoverflow.com/questions/tagged/loona"
               target="_blank"
-              rel="noreferrer noopener">
+              rel="noreferrer noopener"
+            >
               Stack Overflow
             </a>
             <a href="https://spectrum.chat/loona">Spectrum Chat</a>
-            <a
-              href="https://twitter.com/kamilkisiela"
-              target="_blank"
-              rel="noreferrer noopener">
-              Twitter
-            </a>
-          </div>
-          <div>
-            <h5>More</h5>
-            <a href="https://medium.com/the-guild">Blog</a>
             <a href="https://github.com/kamilkisiela/loona">GitHub</a>
-            <a
+            {/* <a
               className="github-button"
               href={this.props.config.repoUrl}
               data-icon="octicon-star"
@@ -70,10 +60,26 @@ class Footer extends React.Component {
               data-count-aria-label="# stargazers on GitHub"
               aria-label="Star this project on GitHub">
               Star
+            </a> */}
+          </div>
+          <div>
+            <h5>Blog &amp; Social</h5>
+            <a href="https://medium.com/the-guild">Medium</a>
+            <a
+              href="https://twitter.com/kamilkisiela"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
+              Twitter
             </a>
           </div>
+          <div>
+            <h5>More</h5>
+            <a href="">FAQ</a>
+            <a href="">Contact &amp; Support</a>
+          </div>
         </section>
-        <section className="copyright">{this.props.config.copyright}</section>
+        {/* <section className="copyright">{this.props.config.copyright}</section> */}
       </footer>
     );
   }
